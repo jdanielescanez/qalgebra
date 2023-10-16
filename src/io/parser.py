53744919a544657e37ee8a53a2f06753a7dbd8d9
Daniel Escanez-Exposito <jdanielescanez@gmail.com>
2023-10-16 12:12:04 +0100

    Add en dash notation

--- src/io/parser.py
+++ src/io/parser.py
@@ -9,13 +9,23 @@ class Parser:
     self.qstate = None
 
   def run(self, text: str) -> list:
-    regex = r'(?P<gate_name>[A-Z]+)(?:(?:\_\{(?P<targets>\d(?:,\d)*)\})|(?:\^\{(?P<controls>\d(?:,\d)*)\}))+'
+    regex = r'(?P<gate_name>[A-Z]+)(?:(?:\_\{(?P<targets>\d+(?:,\d+)*|\d+-\d+)\})|(?:\^\{(?P<controls>\d+(?:,\d+)*|\d+-\d+)\}))+'
     results = re.findall(regex, text)
+    split_string_by_char = lambda string, char: list(map(int, string.split(char)))
 
     for gate_name, targets, controls in results:
-      targets = list(map(int, targets.split(',')))
+      if '-' in targets:
+        first_target, last_target = split_string_by_char(targets, '-')
+        targets = list(range(first_target, last_target + 1))
+      else:
+        targets = split_string_by_char(targets, ',')
+
       if len(controls) > 0:
-        controls = list(map(int, controls.split(',')))
+        if '-' in controls:
+          first_control, last_control = split_string_by_char(controls, '-')
+          controls = list(range(first_control, last_control + 1))
+        else:
+          controls = split_string_by_char(controls, ',')
       else:
         controls = []
 
